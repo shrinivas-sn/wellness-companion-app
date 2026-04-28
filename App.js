0c8b6a3245ecb5cd6108136a490c075c0d6f8024
shrinivas-sn <shrinusn2001@gmail.com>
2026-04-28 17:11:23 +0530

    Resolved package.json issue of expo routing

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import HistoryScreen from "./src/screens/HistoryScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import LogScreen from "./src/screens/LogScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
 
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="History" component={HistoryScreen} />
+        <Tab.Screen name="Log" component={LogScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
